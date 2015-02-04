02f02694c664c7ad034bc111404233f878b0908f
StrongLoop <build@strongloop.com>
2015-02-04 07:27:06 -0500

    sync 2015-02-04 07:27

---
+++ test/test-strong-express-metrics.js
@@ -0,0 +1,42 @@
+var agent = require('../').profile('some app', 'some key');;
+
+var assert = require('assert');
+var http = require('http');
+var express = require('express');
+var metrics = require('strong-express-metrics');
+
+var collected = 0;
+process.on('exit', function() {
+  assert.equal(collected, 1, 'expected: 1 record, actual: ' + collected);
+});
+
+agent.on('express:usage-record', function(record) {
+  collected++;
+  // NOTE(bajtos) Move the assertion to a new tick (stack), because
+  // express-metrics catches (and ignores) errors thrown by record observers
+  // and the test never fails as a result.
+  setImmediate(function() {
+    assert.deepEqual(
+      Object.keys(record).sort(),
+      ['client', 'data', 'process', 'request', 'response', 'timestamp']);
+  });
+});
+
+var app = express();
+app.use(metrics());
+
+var server = app.listen(0, '127.0.0.1', function() {
+  http.get(
+    {
+      host: this.address().address,
+      port: this.address().port,
+      path: '/',
+    },
+    function(res) {
+      res.resume();
+      res.on('end', function() {
+        server.close();
+      });
+    }
+  );
+});
